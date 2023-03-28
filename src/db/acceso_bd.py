f79f60253ae775c674b27c2d755f94cbfe3fef03
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-03-28 23:46:20 +0200

    mysql connection

--- src/db/acceso_bd.py
+++ src/db/acceso_bd.py
@@ -1,21 +1,19 @@
-import pyodbc
+import mysql.connector
 
-# Variables que tienes que configurar según los 
-# datos de acceso a la base de datos que hayas creado
-server = 'Localhost,1433' 
-database = 'Trabajadores' 
-username = 'sa' 
-password = '1234567890' 
+
 
 # Variable donde se encuentra el contexto de la conexión a la base de datos
 try:
-    conexion = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+    conexion = mysql.connector(host='localhost',
+                               database='Trabajadores',
+                               user='root',
+                               password='')
 
     # Objeto cursor para manipular los resultados de las consultas a la base de datos. 
     # Pêrmite enviar consultas y recibir los resultados.
     cursor = conexion.cursor()
 
-except pyodbc.Error as e:
+except mysql.connector.Error as e:
     print("Error al conectar a la base de datos:", e)
 
 
